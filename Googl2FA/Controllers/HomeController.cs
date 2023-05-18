4185283092d369806f32e967c525a9dd0f452e9d
Mohammad Nadeem <nadeem@protrans.com>
2023-05-18 07:29:36 +0530

    Clean up

--- Googl2FA/Controllers/HomeController.cs
+++ Googl2FA/Controllers/HomeController.cs
@@ -29,11 +29,6 @@ namespace Googl2FA.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
